ba2ba4d9e92480aebeee4220e218459ac22f0fd6
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-04-30 14:02:43 +0300

    Add files via upload

---
+++ Source Code/UrAnIdiot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace YouAreAnIdiot
+{
+    public partial class UrAnIdiot : Form
+    {
+        SoundPlayer uranidiot;
+        public void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+        {
+            //This is Very Important Code... DON'T CHANGE THIS!!! 
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            using (Stream s = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+            using (BinaryReader r = new BinaryReader(s))
+            using (FileStream fs = new FileStream(outDirectory + "\\" + resourceName, FileMode.OpenOrCreate))
+            using (BinaryWriter w = new BinaryWriter(fs))
+                w.Write(r.ReadBytes((int)s.Length));
+        }
+        public UrAnIdiot()
+        {
+            string x = @"C:\Temp";
+            Directory.CreateDirectory(x);
+            Extract("YouAreAnIdiot", x, "Resources", "YouAreAnIdiot.wav");
+            InitializeComponent();
+            uranidiot = new SoundPlayer(@"C:\Temp\YouAreAnIdiot.wav");
+            uranidiot.PlayLooping();
+        }
+
+        private void UrAnIdiot_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
